fb8da42d7fe18e4c1cdaa36443c0191a2b066e19
hiep294 <quanghiep294@gmail.com>
2021-06-22 15:01:30 +0700

    index

---
+++ Config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database
+{
+    private static $bdd = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getBdd()
+    {
+        if (is_null(self::$bdd)) {
+            self::$bdd = new PDO("mysql:host=localhost:3306;dbname=mvc", 'root', 'root');
+        }
+        return self::$bdd;
+    }
+}
