fb9c3200b6bb54ec9fe791654b01299d4ea53566
hiep294 <quanghiep294@gmail.com>
2021-06-25 16:19:08 +0700

    sql

--- mvc.sql
+++ mvc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 22, 2019 at 10:48 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.1.19
+-- Host: localhost:3306
+-- Generation Time: Jun 25, 2021 at 09:18 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -41,9 +41,28 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`title`, `description`, `created_at`, `updated_at`, `id`) VALUES
-('task 1', 'this is description for task 1', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1),
-('task 2', 'des for task 2 eee', '2019-04-08 23:40:13', '2019-04-11 04:12:15', 2),
-('task 3', 'des for task 3', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 3);
+('qwe', 'ádassd', '2021-06-25 01:22:16', '2021-06-25 01:22:16', 21);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `title` varchar(255) NOT NULL,
+  `user` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`title`, `user`, `created_at`, `updated_at`, `id`) VALUES
+('title', 'hiep', '2021-06-25 01:54:24', '2021-06-25 01:54:24', 11);
 
 --
 -- Indexes for dumped tables
@@ -56,6 +75,12 @@ ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,7 +88,13 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
